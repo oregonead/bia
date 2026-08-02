2ecb757b206c6b97060c7a77469e9f19a0dee00d
Henrylle Maia <henrylle@oregonead.com.br>
2026-08-02 17:53:35 +0000

    ajuste no botao

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Task com CICD
+        Adicionar Task com CICD V1
       </button>
       
       <Modal
